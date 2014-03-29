8d30a52f46582132b4534ff725bcb3a80bd9dd87
Matt Edwards <matt@emptyhead.com.au>
2014-03-29 12:29:56 +1100

    Remove Analytics
    Signed-off-by: Matt Edwards <matt@emptyhead.com.au>

--- header.php
+++ header.php
@@ -24,22 +24,7 @@
 	<link rel="shortcut icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/icon.png">
 	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">
 
-
 	<?php wp_head(); ?>
-	
-	<script type="text/javascript">
-
-	  var _gaq = _gaq || [];
-	  _gaq.push(['_setAccount', 'UA-49247743-1']);
-	  _gaq.push(['_trackPageview']);
-	
-	  (function() {
-	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	    ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
-	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	  })();
-	
-	</script>
 	
 </head>
 
